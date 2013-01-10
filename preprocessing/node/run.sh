33daaf7bb34b80fdc99fe686f88b37f72939b2b6
Wojciech Gawronski <afronski@gmail.com>
2013-01-10 11:52:20 +0100

    Modified automation scripts

--- preprocessing/node/run.sh
+++ preprocessing/node/run.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-node main.js -D --ommitNumbers -L 4 -q              > results/TFIDF-per-doc
+N=4
+node main.js -D --ommitNumbers -L $N -q              > results/TFIDF-per-doc
 
-node main.js -C course --ommitNumbers -L 4 -q       > results/TFIDF-course
-node main.js -C department --ommitNumbers -L 4 -q   > results/TFIDF-department
-node main.js -C faculty --ommitNumbers -L 4 -q      > results/TFIDF-faculty
-node main.js -C other --ommitNumbers -L 4 -q        > results/TFIDF-other
-node main.js -C projecti --ommitNumbers -L 4 -q     > results/TFIDF-project
-node main.js -C staff --ommitNumbers -L 4 -q        > results/TFIDF-staff
-node main.js -C student --ommitNumbers -L 4 -q      > results/TFIDF-student
+node main.js -C course --ommitNumbers -L $N -q       > results/TFIDF-course
+node main.js -C department --ommitNumbers -L $N -q   > results/TFIDF-department
+node main.js -C faculty --ommitNumbers -L $N -q      > results/TFIDF-faculty
+node main.js -C other --ommitNumbers -L $N -q        > results/TFIDF-other
+node main.js -C projecti --ommitNumbers -L $N -q     > results/TFIDF-project
+node main.js -C staff --ommitNumbers -L $N -q        > results/TFIDF-staff
+node main.js -C student --ommitNumbers -L $N -q      > results/TFIDF-student

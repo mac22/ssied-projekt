17ce12291883ac94b499054471431dd4959dc1ed
Wojciech Gawronski <afronski@gmail.com>
2013-01-10 08:36:33 +0100

    Changing input scripts

--- preprocessing/node/run.sh
+++ preprocessing/node/run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-node main.js -D -q > results/TFIDF-per-doc
+node main.js -D --ommitNumbers -L 4 -q              > results/TFIDF-per-doc
 
-node main.js -C course -q > results/TFIDF-course
-node main.js -C department -q > results/TFIDF-department
-node main.js -C faculty -q > results/TFIDF-faculty
-node main.js -C other -q > results/TFIDF-other
-node main.js -C project -q > results/TFIDF-project
-node main.js -C staff -q > results/TFIDF-staff
-node main.js -C student -q > results/TFIDF-student
+node main.js -C course --ommitNumbers -L 4 -q       > results/TFIDF-course
+node main.js -C department --ommitNumbers -L 4 -q   > results/TFIDF-department
+node main.js -C faculty --ommitNumbers -L 4 -q      > results/TFIDF-faculty
+node main.js -C other --ommitNumbers -L 4 -q        > results/TFIDF-other
+node main.js -C projecti --ommitNumbers -L 4 -q     > results/TFIDF-project
+node main.js -C staff --ommitNumbers -L 4 -q        > results/TFIDF-staff
+node main.js -C student --ommitNumbers -L 4 -q      > results/TFIDF-student
